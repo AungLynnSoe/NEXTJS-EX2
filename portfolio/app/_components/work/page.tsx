255ac7c2b9afb934bb3b93185275d698b0e38d5b
Luffy <w24002@osfl.ac.jp>
2025-06-12 10:38:55 +0900

    workPage update

--- portfolio/app/_components/work/page.tsx
+++ portfolio/app/_components/work/page.tsx
@@ -11,15 +11,15 @@ export default function WorkPage() {
       <ul className={styles.workList}>
         <li className={styles.workItem}>
           <h3>作品名 1</h3>
-          <p>Next.js を使ったポートフォリオサイト</p>
+          <p>一年生の時に作ったサイトです</p>
           <Link href="https://github.com/AungLynnSoe/TRAVELLING" className={styles.link}>
             GitHub リポジトリはこちら
           </Link>
         </li>
         <li className={styles.workItem}>
           <h3>作品名 2</h3>
-          <p>React + Firebase で作った TODO アプリ</p>
-          <Link href="https://github.com/username/project2" className={styles.link}>
+          <p>Next.js を使ったポートフォリオサイト</p>
+          <Link href="https://github.com/AungLynnSoe/NEXTJS-EX2/tree/main/portfolio" className={styles.link}>
             GitHub リポジトリはこちら
           </Link>
         </li>
