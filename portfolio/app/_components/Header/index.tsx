7fec9f0705b9cbaaf87765c6b4f31c31eb5a31f3
Luffy <w24002@osfl.ac.jp>
2025-06-13 11:07:15 +0900

    Header hamburger

--- portfolio/app/_components/Header/index.tsx
+++ portfolio/app/_components/Header/index.tsx
@@ -1,9 +1,22 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import styles from "./index.module.css";
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className={styles.header}>
-      <nav className={styles.navLinks}>
+      {/* <div className={styles.logo}>MySite</div> */}
+      <button className={styles.hamburger} onClick={toggleMenu}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </button>
+      <nav className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
         <ul>
           <li>
             <Link href="/">Home</Link>{" "}
