d57ba761c3a6d8d9f1366decae63060b3a06eac8
Luffy <w24002@osfl.ac.jp>
2025-08-01 11:46:03 +0900

    order app add work

--- portfolio/app/_components/work/page.tsx
+++ portfolio/app/_components/work/page.tsx
@@ -11,11 +11,10 @@ export default function WorkPage() {
       </p>
       <ul className={styles.workList}>
         <li className={styles.workItem}>
-          <h3>作品名 1</h3>
+          <h3>国の有名な場所紹介</h3>
           <p>
-            一年生の時に作ったサイトです。
-            HTMLとCSSの基礎を学びながら制作しました。
-            レイアウトや配色、ナビゲーションの作り方など、Web制作の基本を実践的に身につけた初めての作品です。
+            国の有名な場所を紹介するサイトです。
+            HTML, CSS、JavaScriptを使ったWebサイトの制作に取り組んでおり、最近はNext.jsにも挑戦しています。
           </p>
           <Link
             href="https://travelling-topaz.vercel.app/"
@@ -25,7 +24,7 @@ export default function WorkPage() {
           </Link>
         </li>
         <li className={styles.workItem}>
-          <h3>作品名 2</h3>
+          <h3>portfolio</h3>
           <p>
             Next.js を使ったポートフォリオサイトです。
             ReactフレームワークであるNext.jsを使い、ルーティングやコンポーネント設計、レスポンシブ対応などを意識して開発しました。自分のスキルや作品を紹介するために作成しました。
@@ -35,6 +34,20 @@ export default function WorkPage() {
             className={styles.link}
           >
             GitHub リポジトリはこちら
+          </Link>
+        </li>
+        <li className={styles.workItem}>
+          <h3>order app</h3>
+          <p>
+            Next.js を使ったオーダーアプリです。
+            ユーザーが商品を選択し、注文を行うためのインターフェースを提供します。
+            フロントエンドはNext.jsで構築されており、バックエンドはAPIを通じて連携しています。
+          </p>
+          <Link
+            href="https://order-app-henna.vercel.app/"
+            className={styles.link}
+          >
+            vercel のリンクはこちら
           </Link>
         </li>
         {/* 必要に応じてリストを追加 */}
